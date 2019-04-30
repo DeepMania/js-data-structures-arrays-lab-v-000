79a8897a50394301b38da32a7ec76d247fac1db2
Yana Ustsinovich <yustinovich@icloud.com>
2019-04-30 05:59:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,3 +16,7 @@ function appendDriver(name) {
   let newDrivers = [...drivers, name];
   return newDrivers;
 }
+function prependDriver(name) {
+  let newDrivers = [name, ...drivers];
+  return newDrivers;
+}
